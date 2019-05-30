f38e54fe667272e779dec1046e3c2ddb2b5f8c19
John <jrudd2@anglianwater.co.uk>
2019-05-30 06:15:57 +0100

    feat: added the search page and controller methods

---
+++ GitHubMvc/Areas/Search/Controllers/SearchGitHubController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using GitHubMvc.Areas.Search.Models;
+
+namespace GitHubMvc.Areas.Search.Controllers
+{
+    public class SearchGitHubController : Controller
+    {
+        [HttpPost]
+        public async Task<ActionResult> Index(SearchModel model)
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("User-Agent", "BGL-Technicial-Test");
+
+                var url = new UriBuilder("https://api.github.com/users/robconery");
+
+                var response = await client.GetAsync(url.ToString());
+                var result = await response.Content.ReadAsStringAsync();
+                return Content(result, "application/json");
+
+            }
+        }
+    }
+}
---
+++ GitHubMvc/Areas/Search/Models/SearchModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GitHubMvc.Areas.Search.Models
+{
+    public class SearchModel
+    {
+        [Required]
+        public string Name { get; set; }
+    }
+}
